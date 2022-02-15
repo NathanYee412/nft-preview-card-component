1533b268b19cd340dbffc20a70ab38fbda16019a
Nathan Yee <nyee@csu.fullerton.edu>
2022-02-15 09:35:31 -0800

    tailwind added and completed styling

--- src/components/NftPreviewCard.js
+++ src/components/NftPreviewCard.js
@@ -1,4 +1,3 @@
-import './NftPreviewCard.css';
 import nft from '../assets/images/image-equilibrium.jpg';
 import ethlogo from '../assets/images/icon-ethereum.svg';
 import clockicon from '../assets/images/icon-clock.svg';
@@ -8,23 +7,28 @@ function NftPreviewCard(props) {
 
     return(
         <div className='container'>
-            <div className='card'>
-                <img src={nft} alt='nft' className='nft' />
-                <h1 className='title'>{props.title}</h1>
-                <p className='desc'>{props.desc}</p>
-                <div className='priceandtime'>
-                    <div className='left'>
-                        <img src={ethlogo} alt='ETH logo' />
-                        <p className='price'>{props.ethprice}ETH</p>
-                    </div>
-                    <div className='right'>
-                        <img src={clockicon} alt='clock icon' />
-                        <p className='time'>{props.timeleft}</p>
+            <div className='bg-[#15273f] w-[400px] mx-auto mt-5 rounded-xl'>
+                <div className='w-[350px] mx-auto py-[24px] drop-shadow-lg'>
+                    <img src={nft} alt='nft' className='rounded-xl'/>
+                </div>
+                <h1 className='text-white text-2xl font-bold px-[25px]'>{props.title}</h1>
+                <p className='text-[#90abd4] text-lg font-semibold px-[25px] pt-5'>{props.desc}</p>
+                <div className='grid grid-cols-2 px-[25px] pt-5'>
+                    <div className='flex'>
+                        <img src={ethlogo} alt='ETH logo' className='self-center mb-4' />    
+                        <p className='text-[#24f2ed] text-md font-semibold pl-2'>{props.ethprice} ETH</p>
+                    </div>    
+                    <div className='flex pb-4'>
+                        <img src={clockicon} alt='clock icon' className='self-center' />
+                        <p className='text-[#90abd4] text-md font-semibold pl-2'>{props.timeleft} Days Left</p>
                     </div>
                 </div>
-                <div className='logovalue'>
-                    <img src={avatar} alt='avater' className='avatar' />
-                    <p>Creation of {props.author}</p>
+                <hr className='mx-[25px] border-[#90abd4]'/>
+                <div className='flex px-[25px] py-4'>
+                    <div className='pr-5'>
+                        <img src={avatar} alt='avatar' className='rounded-full border-2 border-white h-[40px]' />
+                    </div>
+                    <p className='my-auto text-[#90abd4] text-md font-semibold'>Creation of <span className='text-white'>{props.author}</span></p>
                 </div>
             </div>
         </div>
